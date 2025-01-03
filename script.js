2a545b9e4ca42d7fa650fd248ebc905df68943e4
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-03 15:14:36 -0500

    created button variable

---
+++ script.js
@@ -0,0 +1,5 @@
+//create a variable for the button
+//user first selects a city
+//then user clicks the button 
+//to retrieve weather information
+let btn = document.getElementById("get-forecast");
