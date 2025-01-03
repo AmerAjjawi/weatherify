6810d9897ef765a4cc78649b5a64763877057313
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-03 15:50:30 -0500

    created comments with a to do list to work on

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-//create a variable for the button
+//create a button variable for the button
 //user first selects a city
 //then user clicks the button 
 //to retrieve weather information
 let btn = document.getElementById("get-forecast");
+
+
+//create a select variable
+//the purpose of this select variable
+//is to hold the values of the options
+//that you can loop through 
+//and retrieve values 
