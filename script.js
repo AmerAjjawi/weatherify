ec283f0adb53ad7d1426c15bbf673e01d22121f0
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-04 17:20:43 -0500

    called btn variable with event listener to loop through the nodelist and display select and option elements using for...of

--- script.js
+++ script.js
@@ -5,26 +5,53 @@
 let btn = document.getElementById("get-forecast");
 
 
-//create a select variable
-//the purpose of this select variable
-//is to hold the values of the options
-//that you can loop through 
-//and retrieve values 
 
 
 
-//to do: 
-//figure out how to set up the select-option values
-//either queryselectorall, queryselector, id
-//figure out how to store them
+//set up varible to grab the select option
+//going to use a selectorqueryAll to return a nodelist
+let selectedItem = [...document.querySelectorAll(".city")];
+
+//let cityOption;
+//use case: 
+//return the nodelist of options
+
+//1) create an event listner
+ btn.addEventListener("click", (event) => {
+//   
+    console.log(selectedItem);
+//     //use for...of loop to loop in sequence of iterable nodelist
+     for (selectedItems of selectedItem) {
+        console.log(selectedItems);
+        
+       return selectedItems;
+     }
+    
+ })
+
+
+//January 4th, what I did was use querySelectorAll()
+//with a spread operator to show it in key value pairs 
+//I then called the button variable with an event listener
+//of click and event 
+//to display the select element and it's options
+
+
+//use array form
+//use .foreach
+
+
+
+
+
+ //to do:
 //figure out how to loop through them
 //figure out how to display them
 //figure out the conditionals
 //i.e if no city is selected, then value is not shown
 //i.e if city is selected, then display it
+//i need to show how each option individually selected 
 
 
-//set up varible to grab the select option
-//going to use a selectorqueryAll to create a nodelist
-let selectedItem = document.querySelectorAll(".city");
-console.log(selectedItem);
+
+
