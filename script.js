9e3257b0c5715957681213d0e2dcaeb2acd9a3c0
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-05 21:39:44 -0500

    Tried different methods of grabbing the select element option

--- script.js
+++ script.js
@@ -10,25 +10,35 @@ let btn = document.getElementById("get-forecast");
 
 //set up varible to grab the select option
 //going to use a selectorqueryAll to return a nodelist
-let selectedItem = [...document.querySelectorAll(".city")];
-
-//let cityOption;
-//use case: 
-//return the nodelist of options
-
-//1) create an event listner
- btn.addEventListener("click", (event) => {
-//   
+  let selectedItem = [...document.querySelectorAll(".city")];
     console.log(selectedItem);
-//     //use for...of loop to loop in sequence of iterable nodelist
-     for (selectedItems of selectedItem) {
-        console.log(selectedItems);
-        
-       return selectedItems;
-     }
-    
- })
 
+
+//  let selectedItem = document.getElementById("#city");
+
+// function getOptionValue(selectedItem) {
+//     let array = [];
+//     for (let i = 0; i<selectedItem; i++) {
+//     console.log(array.push(selectedItem.options[selectedItem.selectedIndex].text));
+
+//     }
+    
+// }
+
+
+btn.addEventListener("click", getOptionValue);
+
+// let optionValue = selectedItem.options[selectedItem.selectedIndex].text;
+// console.log(optionValue);
+
+
+//  let array = [];
+
+//  function getSelectedOptions() {
+//     for(let i = 0; i < selectedItem.length; i++) {
+//         console.log(array.push(selectedItem.options[selectedItem.selectedIndex].value));
+//     }
+//  }
 
 //January 4th, what I did was use querySelectorAll()
 //with a spread operator to show it in key value pairs 
@@ -37,20 +47,14 @@ let selectedItem = [...document.querySelectorAll(".city")];
 //to display the select element and it's options
 
 
-//use array form
-//use .foreach
-
-
-
-
 
  //to do:
-//figure out how to loop through them
-//figure out how to display them
-//figure out the conditionals
+
 //i.e if no city is selected, then value is not shown
 //i.e if city is selected, then display it
 //i need to show how each option individually selected 
+
+//try to do selectItems.foreach {}
 
 
 
