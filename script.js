08693a07457a8597751a5e9050b3f98fbe9bc87e
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-17 13:57:31 -0500

    created a variable for the select element and an uninitalized variable for options which will be called in the event listener

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 //user first selects a city
 //then user clicks the button 
 //to retrieve weather information
-let btn = document.getElementById("paragraph");
-console.log(btn);
-//Uninitialized variable for select element options
-let cityList;
+let btn = document.querySelector("#get-forecast");
+// console.log(btn);
 
+//create a variable cityList 
+//return the select element and its options
+let cityList = document.querySelector("#city");
+console.log(cityList);
+
+//create an initialized variable
+//this will be called in the event listener
+let selectedOption;
+
+//create an event listener for the button using "change"
+btn.addEventListener("change", (e) => {
+    //prevent the default action of button
+   e.preventDefault();
+     [].filter
+    .call(cityList.options, option => option.selected)
+    .map(option =)
+})
 
 
 
