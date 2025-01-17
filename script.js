b53b3108fc9f428048c23626d1ed01fdd6c92a56
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-17 18:58:12 -0500

    Tried different methods to grab the option value

--- script.js
+++ script.js
@@ -2,39 +2,26 @@
 //user first selects a city
 //then user clicks the button 
 //to retrieve weather information
-let btn = document.querySelector("#get-forecast");
-// console.log(btn);
+const btn = document.querySelector("#get-forecast");
 
-//create a variable cityList 
-//return the select element and its options
-let cityList = document.querySelector("#city");
-console.log(cityList);
 
-//create an initialized variable
-//this will be called in the event listener
-let selectedOption;
+
+   
+let cityList = [...document.querySelector("city")];
+
+
+
+
+
 
 //create an event listener for the button using "change"
 btn.addEventListener("change", (e) => {
     //prevent the default action of button
    e.preventDefault();
-     [].filter
-    .call(cityList.options, option => option.selected)
-    .map(option =)
+ 
+
+    
+    
 })
-
-
-
-
-
-
-
-
-// btn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     for (citylists of cityList) {
-//       console.log(citylists);
-//     }
-// })
 
 
