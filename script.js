811987856fa6fa4d99b8c63311f6bf8de5bad98b
Amer Ajjawi <ajjawi_amer@hotmail.com>
2025-01-25 15:52:37 -0500

    removed the getValues and showWeather functions. Placed the select option values inside the asynch fetch to retrieve with an api

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+//Declare gloabl variables
+
+
 //create a button variable for the button
 const btn = document.querySelector("#get-forecast");
 //variable for the weather card
@@ -6,27 +10,20 @@ const card = document.getElementById("card");
 //variable to call the select element id
 const cityList = document.querySelector("#city");
 
-
-
-
-// // //variable for Api key
-// const key = "50aaa7216a3648d495712656241511";
-// // //variable for Api base    
-// let base = `https://api.weatherapi.com/vi/current.json/Key=${key}&q=london`
-
-
-
-
+// let cityValues = cityList.value;
+// console.log(cityValues);
 
 //async function to fetch a response  
 async function getData() {
     //try block to test the response
     try {
-        //use fetch and await to get the data
-        const response = await fetch(url)
+        let cityValues = cityList.value;
+        console.log(cityValues);
+        const key = '50aaa7216a3648d495712656241511';
+        const response = await fetch()
         //console log response
-        console.log(response)   
-         
+        console.log(response)
+
         //if response is not okay or 200
         if (!response.ok) {
             //throw a new error
@@ -34,71 +31,59 @@ async function getData() {
         }
         //if it works then return the data in .json
         return response.json();
-     }
-     //catch the error
+    }
+    //catch the error
     catch {
         console.log(error);
     }
 }
 
-//call the API and call the getData() function
-async function getWeather() {
-let cityValues = cityList.value;
- const ShowWeather = await getData(`http://api.weatherapi.com/v1/current.json?Key=50aaa7216a3648d495712656241511&q=${cityValues}`, { mode: 'cors' })
- console.log(ShowWeather);
 
+// function getValues() {
 
-  getValues()
-}
-getWeather();
+//     //variable to return the select option values
+//     // let cityValues = cityList.value;
+//     if (cityValues) {
+//         //display the weather icon
+//         let img = document.createElement("image");
+//         //display the city
+//         let location = document.createElement("h1");
+//         //display the temperature
+//         let temp = document.createElement("p");
+//         //display feels-like
+//         let feelsLike = document.createElement("p");
+//         //display humidity
+//         let humidity = document.createElement("p");
+//         //dispaly wind
+//         let wind = document.createElement("p");
 
-
-
-function getValues() {
-
-    //variable to return the select option values
-    let cityValues = cityList.value;
-    if (cityValues) {
-        //display the weather icon
-        let img = document.createElement("image");
-        //display the city
-        let location = document.createElement("h1");
-        //display the temperature
-        let temp = document.createElement("p");
-        //display feels-like
-        let feelsLike = document.createElement("p");
-        //display humidity
-        let humidity = document.createElement("p");
-        //dispaly wind
-        let wind = document.createElement("p");
-
-        //set variable IDs
-        img.id = "weather-icon";
-        location.id = "location";
-        temp.id = "main-temperature";
-        feelsLike.id = "feels-like"
-        humidity.id = "humidity";
-        wind.id = "wind";
+//         //set variable IDs
+//         img.id = "weather-icon";
+//         location.id = "location";
+//         temp.id = "main-temperature";
+//         feelsLike.id = "feels-like"
+//         humidity.id = "humidity";
+//         wind.id = "wind";
 
 
 
 
-        //assign value to the variable
-        location.textContent = cityValues
+//         //assign value to the variable
+//         location.textContent = cityValues
 
-        //append variables
-        // card.append(img);
-        card.append(location);
-
-
-
-    }
+//         //append variables
+//         // card.append(img);
+//         card.append(location);
 
 
-}
+
+//     }
+
+
+// }
 
 
 
 //create an event listener for the button using click
-btn.addEventListener("click", getValues);
+btn.addEventListener("click", getData);
 
